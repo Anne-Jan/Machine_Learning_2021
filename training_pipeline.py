3e9c03e47ff131034ef6cc1b6f4be8dbb8e3485f
Anne-Jan <anne-janmein@hotmail.com>
2022-02-01 13:23:38 +0100

    working model on 2d images

--- training_pipeline.py
+++ training_pipeline.py
@@ -15,14 +15,21 @@ from skimage import io
 from augmentations import *
 
 
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+
+
 ####Paramaters for data augmentation####
-#Value that determines by how much the data augmentation method should create new data
-data_multiplier = 4
+
 #Value that determines the chance that a pixel in an image is changed to noise
 pixel_aug_chance = 0.15
 
 num_rot = 2 # number of rotated images to augment from each original image
 num_noise = 2 # number of noise image augmented from each original image
+
+#Value that determines by how much the data augmentation method should create new data
+data_multiplier = num_rot + num_noise
 
 ####Parameters for the CNN####
 #Activation function
@@ -48,10 +55,6 @@ with open('mnist_digits_data.txt') as f:
 
 data = np.asarray(data)
 
-#labels are 0-9, 200 of each in the base dataset
-labels = np.zeros(200 * data_multiplier)
-for i in range (1, 10):
-  labels = np.concatenate((labels, np.zeros(200 * data_multiplier)+i))
 
 #Create a second set of labels for the original dataset
 labels_original_data = np.zeros(200)
@@ -59,13 +62,15 @@ for i in range (1, 10):
   labels_original_data = np.concatenate((labels_original_data, np.zeros(200)+i))
 
 
-print(labels)
+# print(labels)
 #Normalize the pixel values between 0-1
 data = data / np.amax(data)
 
 
 #Create data to be multidimensional
 data = np.reshape(data, (2000, 16, 15, 1))
+
+print("Shape of original 2d data = " + str(data.shape))
 
 ###Old data augmentation
 # new_data = []
@@ -97,13 +102,15 @@ for image in data:
         noise_img = random_noise(image)
         data_augmented.append(rot_img)
 
-data_augmented = np.array(data_augmented)
+data_augmented = np.asarray(data_augmented)
 print("Shape of augmented 2d data = " + str(data_augmented.shape))
 
 #labels for augmented data
-labels_aug = np.zeros(200 * int(data_augmented.shape[0]/len(data)))
+#labels are 0-9, 200 of each in the base dataset
+labels_aug = np.zeros(200 * data_multiplier)
 for i in range (1, 10):
-  labels_aug = np.concatenate((labels_aug, np.zeros(200* int(data_augmented.shape[0]/len(data)))+i))
+  labels_aug = np.concatenate((labels_aug, np.zeros(200 * data_multiplier)+i))
+
 
 #Define the Kfold Cross Validation Model
 folds = 10
@@ -114,14 +121,15 @@ loss_per_fold = []
 model_per_fold = []
 
 current_fold = 1
-for train, test in kfold.split(data_augmented, labels):
+for train, test in kfold.split(data_augmented, labels_aug):
 
 
   #gepakt van een tutorial, moeten we aanpassen
   model = keras.Sequential([
-      keras.layers.Conv2D(filters = 32, kernel_size = (3,3), activation = activation, input_shape = (16,15,1)),
-      keras.layers.MaxPooling2D(2,2),
-      keras.layers.Dense(10, activation= activation),
+      keras.layers.Conv2D(filters = 64, kernel_size = (3,3), activation = activation, input_shape = (16, 15, 1)),
+      keras.layers.MaxPooling2D((2,2)),
+      keras.layers.Flatten(),
+      keras.layers.Dense(64, activation= activation),
       keras.layers.Dense(10, activation='softmax')
       #add layers
   ])
@@ -134,9 +142,11 @@ for train, test in kfold.split(data_augmented, labels):
   print("###################################")
   print("Fold Number:" + str(current_fold))
 
-  history = model.fit(data[train], labels[train], epochs = 10)
+  print(data_augmented[train].shape)
+  print(labels_aug[train].shape)
+  history = model.fit(data_augmented[train], labels_aug[train], epochs = 10)
   model_per_fold.append(model)
-  score = model.evaluate(data[test], labels[test], verbose = 0)
+  score = model.evaluate(data_augmented[test], labels_aug[test], verbose = 0)
 
   accuracy_per_fold.append(score[1] * 100)
   loss_per_fold.append(score[0])
@@ -146,14 +156,16 @@ for idx in range(current_fold - 1):
   print("Accuracy for fold: " + str(idx + 1)+ " = " + str(accuracy_per_fold[idx]))
   print("Loss for fold: " + str(idx + 1)+ " = " + str(loss_per_fold[idx]))
 
+
+
 #take the model with the highest accuracy from the cross validation
 best_model = model_per_fold[accuracy_per_fold.index(max(accuracy_per_fold))]
 
 #shuffle the original data and its labels
-original_data, labels_original_data = shuffle(original_data, labels_original_data, random_state = 0)
+data, labels_original_data = shuffle(data, labels_original_data, random_state = 0)
 
 #evaluate the model best on the original data
-score_original_data = model.evaluate(original_data, labels_original_data, verbose = 0)
+score_original_data = model.evaluate(data, labels_original_data, verbose = 0)
 print("################################################")
 print("Accuracy on original data = " + str(score_original_data[1] * 100))
 print("Loss on original data = " + str(score_original_data[0]))
